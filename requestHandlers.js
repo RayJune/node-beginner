bc41ac695ed5fa34a4e5b5f270c19885459a7e80
rayjune <ray_june@qq.com>
2017-05-26 09:25:17 +0800

    route to real processing program

---
+++ requestHandlers.js
@@ -0,0 +1,12 @@
+function start() {
+  console.log("Request handler 'start' was called.");
+}
+
+function upload() {
+  console.log("Request handler 'upload' was called.");
+}
+
+exports.start = start;
+exports.upload = upload;
+
+//路由目标的函数: 称为请求处理程序
